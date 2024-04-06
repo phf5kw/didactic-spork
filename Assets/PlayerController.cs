9b0e82e6de447466e1cad0766f2c91c182fc2e60
Catherine Xu <phf5kw@virginia.edu>
2024-04-06 18:30:24 -0400

    project base

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    public float jumpForce = 400f;
+    public float slipForce = 100f
+    Rigidbody2D rb;
+
+    // Start called on start
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+    }
+
+    // your Jump code here
+    private void OnJump(InputValue value)
+    {
+        if (value.isPressed)
+        {
+            rb.AddForce(Vector3.up * rb.mass * jumpForce);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.AddForce(Vector3.down * rb.mass * slipForce);
+    }
+}
