371464635e835ccafc2de9aa094de2433afcadad
Catherine Xu <phf5kw@virginia.edu>
2024-04-07 13:39:40 -0400

    wall jumping basic

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -5,14 +5,19 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    public float jumpForce = 400f;
-    public float slipForce = 100f
+    public float jumpForce = 3.0f;
+    public float leapSpeed = 2.0f;
+    public bool dir_right = true;
+    public float wallGrav = 0.05f;
+    public float gravityScale = 1f;
     Rigidbody2D rb;
+    BoxCollider2D bc;
 
     // Start called on start
     void Start()
     {
         rb = this.GetComponent<Rigidbody2D>();
+        bc = this.GetComponent<BoxCollider2D>();
     }
 
     // your Jump code here
@@ -20,13 +25,57 @@ public class PlayerController : MonoBehaviour
     {
         if (value.isPressed)
         {
-            rb.AddForce(Vector3.up * rb.mass * jumpForce);
+            gravityScale = 1f;
+            if (dir_right)
+            {
+                rb.velocity = new Vector2(leapSpeed, jumpForce);
+                //rb.AddForce(transform.up * jumpForce, transform.right * jumpForce);
+            } else
+            {
+                rb.velocity = new Vector2(-leapSpeed, jumpForce);
+                //rb.AddForce(transform.up * jumpForce, transform.left * jumpForce);
+            }
+            
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        Debug.Log("Collide");
+        if (col.gameObject.tag == "Right Wall")
+        {
+            rb.velocity = new Vector2(0, 0);
+            gravityScale = wallGrav;
+            Debug.Log("Hit right wall");
+            dir_right = false; //changes player direction
+        }
+        else if (col.gameObject.tag == "Left Wall")
+        {
+            rb.velocity = new Vector2(0, 0);
+            gravityScale = wallGrav;
+            Debug.Log("Hit left wall");
+            dir_right = true; //changes player direction
+        }
+    }
+
+    void OnCollisionStay2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Right Wall") {
+            Debug.Log("On right wall");
+        } else if (col.gameObject.tag == "Left Wall")
+        {
+            Debug.Log("On left wall");
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-        rb.AddForce(Vector3.down * rb.mass * slipForce);
+    }
+
+    // Used for physics
+    void FixedUpdate()
+    {
+        rb.gravityScale = 2f * gravityScale;
     }
 }
